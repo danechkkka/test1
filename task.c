c3afd384047b18fe17c8ee33ef11161f0a788c3f
Alexandr <a.isakov@kickecosystem.com>
2025-12-26 13:32:00 +0200

    add checking of index array

--- task.c
+++ task.c
@@ -2,7 +2,11 @@ void main(void) {
 	int i, index,a[5];
 	for ( i = 0; i < 5; i++)
 		a[i] = rand() % 10;
-	printf("Введите index элемент\n");
+    printf("Введите index элемент\n");
 	scanf_s("%d", &index);
-	printf("%d элемент  |  %d", index, a[index]);
+    if(index<0 || index>4)
+        printf("Ошибка: индекс вне диапазона\n");
+    
+    else 
+		printf("%d элемент  |  %d", index, a[index]);
 }
